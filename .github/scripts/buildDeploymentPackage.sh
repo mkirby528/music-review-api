b2dd601fab5095e5987ef80f51f8ed8431886462
Matthew Kirby <mkirby2899@gmail.com>
2023-03-20 22:43:46 -0400

    Delete buildDeploymentPackage.sh

--- .github/scripts/buildDeploymentPackage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install  --target ./package -r src/requirements.txt
-echo "Creating deployment package..."
-cd package
-zip -r ../archive.zip .
-cd .. 
-zip archive.zip -r src/
