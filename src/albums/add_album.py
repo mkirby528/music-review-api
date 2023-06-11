141489dc7589a5f5ec87cd44a9461c0d835a1900
Matthew Kirby <mkirby2899@gmail.com>
2023-06-11 13:35:06 -0400

    build artist string in add_album

--- src/albums/add_album.py
+++ src/albums/add_album.py
@@ -1,3 +1,10 @@
 def add_album_to_db(table, album):
-    print(album)
+    album["ArtistsString"] = ", ".join(get_artists(album))
     return table.put_item(Item=album)
+
+
+def get_artists(album):
+    artists = []
+    for artist in album["artists"]:
+        artists.append(artist["name"])
+    return artists
