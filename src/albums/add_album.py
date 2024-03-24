06f5974dfc61b35c746bac165cb38b30bb690f5c
Matthew Kirby <mkirby528@matthews-air.mynetworksettings.com>
2024-03-24 15:01:18 -0400

    turn off openai

--- src/albums/add_album.py
+++ src/albums/add_album.py
@@ -3,5 +3,5 @@ from src.openai.album_review import generate_review
 
 def add_album_to_db(table, album):
     album["ArtistsString"] = ", ".join(album["Artists"])
-    album["OpenAIReview"] = generate_review(album)
+    album["OpenAIReview"] = ""
     return table.put_item(Item=album)
